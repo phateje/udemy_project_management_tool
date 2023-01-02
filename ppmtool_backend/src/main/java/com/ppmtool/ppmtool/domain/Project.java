e016722515c065e534e2dec75f7bfcb43cc1e33e
phateje <phateje@gmail.com>
2023-01-02 12:42:01 -0800

    added some repositories and created a database entity

--- ppmtool_backend/src/main/java/com/ppmtool/ppmtool/domain/Project.java
+++ ppmtool_backend/src/main/java/com/ppmtool/ppmtool/domain/Project.java
@@ -1,4 +1,85 @@
 package com.ppmtool.ppmtool.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectId;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Date createdAt;
+    private Date updatedAt;
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
---
+++ ppmtool_backend/src/main/java/com/ppmtool/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.ppmtool.ppmtool.repositories;
+
+
+import com.ppmtool.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
