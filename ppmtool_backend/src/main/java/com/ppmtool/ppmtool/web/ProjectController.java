7acce051d74398b3d24e5ea3b4c54a648475ab37
phateje <phateje@gmail.com>
2023-01-03 22:43:02 -0800

    error handling I guess

--- ppmtool_backend/src/main/java/com/ppmtool/ppmtool/web/ProjectController.java
+++ ppmtool_backend/src/main/java/com/ppmtool/ppmtool/web/ProjectController.java
@@ -2,13 +2,18 @@ package com.ppmtool.ppmtool.web;
 
 import com.ppmtool.ppmtool.domain.Project;
 import com.ppmtool.ppmtool.services.ProjectService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.StringJoiner;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,7 +22,16 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> upsert(@RequestBody Project project) {
+    public ResponseEntity<?> upsert(@Valid @RequestBody Project project, BindingResult result) {
+        // binding result just gets populated for you... neat
+        if (result.hasErrors()) {
+            StringJoiner stringJoiner = new StringJoiner("\n - ");
+            stringJoiner.add("Found the following issues with the project:");
+            for (ObjectError error : result.getAllErrors()) {
+                stringJoiner.add(error.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(stringJoiner.toString(), HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<Project>(projectService.upsert(project), HttpStatus.CREATED);
     }
 }
