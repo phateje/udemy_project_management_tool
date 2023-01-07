e3446d7587022242766ccd9d3ff12008682b6104
phateje <phateje@gmail.com>
2023-01-07 14:40:30 -0800

    ain't it fun wasting time to learn to use defunct libraries

--- ppmtool_frontend/src/App.js
+++ ppmtool_frontend/src/App.js
@@ -1,13 +1,21 @@
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+
+        <Routes>
+          <Route exact path="/addProject" element={<AddProject />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ ppmtool_frontend/src/components/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AddProject extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>hey fren, add a project</div>
+      </React.Fragment>
+    );
+  }
+}
