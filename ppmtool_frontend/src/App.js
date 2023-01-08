8bd3ca8e94bd550d94a1714ee372651b941796a4
phateje <phateje@gmail.com>
2023-01-07 19:38:30 -0800

    setup redux in a very obsolete way, but there ya go, course be 5 years old

--- ppmtool_frontend/src/App.js
+++ ppmtool_frontend/src/App.js
@@ -3,19 +3,23 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
 
-        <Routes>
-          <Route exact path="/" element={<Dashboard />}></Route>
-          <Route exact path="/addProject" element={<AddProject />}></Route>
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route exact path="/" element={<Dashboard />}></Route>
+            <Route exact path="/addProject" element={<AddProject />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- ppmtool_frontend/src/store.js
+++ ppmtool_frontend/src/store.js
@@ -1,3 +1,31 @@
+import { initializeConnect } from "react-redux/es/components/connect";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
+const initState = {};
+const middleware = [thunk];
+
+let store;
+
+// this if else is only there to setup stuff for the redux chrome extension and kinda ghastly
+// will try to fix it later, unimportant for now
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    rootReducer,
+    initState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initState,
+    compose(applyMiddleware(...middleware))
+  );
+}
+
+export default store;
