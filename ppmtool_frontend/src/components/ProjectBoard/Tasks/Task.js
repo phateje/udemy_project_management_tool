d22ef0e535c8dfb1c31c153c38bf1d3b23da8472
phateje <phateje@gmail.com>
2023-05-12 16:16:52 -0700

    wired up view + update buttons

--- ppmtool_frontend/src/components/ProjectBoard/Tasks/Task.js
+++ ppmtool_frontend/src/components/ProjectBoard/Tasks/Task.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function Task(props) {
   const task = props.task;
+  const projectId = useParams().projectId;
   return (
     <div className="card mb-1 bg-light">
       <div className="card-header text-primary">
@@ -11,9 +13,12 @@ export default function Task(props) {
       <div className="card-body bg-light">
         <h5 className="card-title">{task.summary}</h5>
         <p className="card-text text-truncate ">{task.acceptanceCriteria}</p>
-        <a href="" className="btn btn-primary">
+        <Link
+          to={`/addTask/${projectId}/${task.projectSequence}`}
+          className="btn btn-primary"
+        >
           View / Update
-        </a>
+        </Link>
 
         <button className="btn btn-danger ml-4">Delete</button>
       </div>
